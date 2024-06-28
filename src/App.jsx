964e48513566fa0cf307411a822ce83d0a949f01
ChNafi222 <chnafi379@gmail.com>
2024-06-28 18:46:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+
+  return (
+    <main className="bg-black">
+      <Navbar>
+
+      </Navbar>
+      <Hero>
+        
+      </Hero>
+    </main>
+  )
+}
+
+export default App
