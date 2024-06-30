137acea4cd4ebfd2d69e013328d1b658b2cea82c
ChNafi222 <chnafi379@gmail.com>
2024-06-30 16:12:58 +0500

    features and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from './components/HowItWorks'
+
+import * as Sentry from '@sentry/react'
+import Footer from "./components/Footer";
 const App = () => {
+
+  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
 
   return (
     <main className="bg-black">
@@ -10,8 +17,11 @@ const App = () => {
       <Hero/> 
       <Highlights/>
       <Model />
+      <Features />
+      <HowItWorks />
+      <Footer/>
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,23 @@ import './index.css'
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://14ab925c91b0b61530bc16584a54efde@o4507516872622080.ingest.us.sentry.io/4507516876357632",
+  dsn: "https://d674932a77e6d9b9ced1190d70fd4691@o4506876178464768.ingest.us.sentry.io/4506876181151744",
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
   ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracesSampleRate: 1.0, 
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
 });
-
-const container = document.getElementById(“app”);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
