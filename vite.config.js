6436287abb25f7e495459ed6697f33bea9e1ebc0
ChNafi222 <chnafi379@gmail.com>
2024-07-01 13:11:15 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,3 @@
-// import { sentryVitePlugin } from "@sentry/vite-plugin";
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), sentryVitePlugin({
-//     org: "devsidyll",
-//     project: "javascript-react"
-//   })],
-//   build: {
-//     sourcemap: true
-//   },
-//   base: '/Apple-3D-Website/',
-// })
-
-
-// ---------------
-
-
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -34,7 +14,6 @@ export default defineConfig(({ mode }) => {
     ],
     build: {
       sourcemap: true
-    },
-    base: mode === 'production' ? '/Apple-3D-Website/' : '/',
+    }
   }
 });
